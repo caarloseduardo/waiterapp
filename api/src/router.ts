4523e430c2364b2c18dce03a3f9a1f4f294846f4
Carlos Eduardo <carloseduardo.and2@gmail.com>
2023-02-27 18:07:07 -0300

    refactor: add files into app folder

--- api/src/router.ts
+++ api/src/router.ts
@@ -1,22 +1,18 @@
 import { Router } from 'express'
+import { createCategory } from './app/use-cases/categories/create-category'
+import { listCategories } from './app/use-cases/categories/list-categories'
+import { createProduct } from './app/use-cases/products/create-product'
+import { listProducts } from './app/use-cases/products/list-products'
 
 export const router = Router()
 
-router.get('/categories', (req, res) => {
-  res.send('OK')
-})
+router.get('/categories', listCategories)
 
-router.post('/categories', (req, res) => {
-  res.send('OK')
-})
+router.post('/categories', createCategory)
 
-router.get('/products', (req, res) => {
-  res.send('OK')
-})
+router.get('/products', listProducts)
 
-router.post('/products', (req, res) => {
-  res.send('OK')
-})
+router.post('/products', createProduct)
 
 router.get('/categories/:categoryId/products', (req, res) => {
   res.send('OK')
