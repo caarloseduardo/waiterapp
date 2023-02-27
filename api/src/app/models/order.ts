88158dec0276a277df4f4ab518429d2320d7d24f
Carlos Eduardo <carloseduardo.and2@gmail.com>
2023-02-27 18:57:31 -0300

    feat(orders): add order use cases

--- api/src/app/models/order.ts
+++ api/src/app/models/order.ts
@@ -3,7 +3,7 @@ import { model, Schema } from 'mongoose'
 export const Order = model(
   'Order',
   new Schema({
-    name: {
+    table: {
       type: String,
       required: true
     },
--- api/src/router.ts
+++ api/src/router.ts
@@ -1,10 +1,28 @@
+import path from 'node:path'
+import multer from 'multer'
 import { Router } from 'express'
 import { createCategory } from './app/use-cases/categories/create-category'
 import { listCategories } from './app/use-cases/categories/list-categories'
 import { createProduct } from './app/use-cases/products/create-product'
 import { listProducts } from './app/use-cases/products/list-products'
+import { listProductsByCategory } from './app/use-cases/categories/list-products-by-category'
+import { listOrders } from './app/use-cases/orders/list-orders'
+import { createOrder } from './app/use-cases/orders/create-order'
+import { changeOrderStatus } from './app/use-cases/orders/change-order-status'
+import { cancelOrder } from './app/use-cases/orders/cancel-order'
 
 export const router = Router()
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, callback) {
+      callback(null, path.resolve(__dirname, '..', 'uploads'))
+    },
+    filename(req, file, callback) {
+      callback(null, `${Date.now()}-${file.originalname}`)
+    }
+  })
+})
 
 router.get('/categories', listCategories)
 
@@ -12,24 +30,14 @@ router.post('/categories', createCategory)
 
 router.get('/products', listProducts)
 
-router.post('/products', createProduct)
+router.post('/products', upload.single('image'), createProduct)
 
-router.get('/categories/:categoryId/products', (req, res) => {
-  res.send('OK')
-})
+router.get('/categories/:categoryId/products', listProductsByCategory)
 
-router.get('/orders', (req, res) => {
-  res.send('OK')
-})
+router.get('/orders', listOrders)
 
-router.post('/orders', (req, res) => {
-  res.send('OK')
-})
+router.post('/orders', createOrder)
 
-router.patch('/orders/:orderId', (req, res) => {
-  res.send('OK')
-})
+router.patch('/orders/:orderId', changeOrderStatus)
 
-router.delete('/orders/:orderId', (req, res) => {
-  res.send('OK')
-})
+router.delete('/orders/:orderId', cancelOrder)
